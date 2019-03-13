f3ab86b0143cb065df08c56aad98f60e0838aafb
Christopher Grabinski <christopher@unbabel.com>
2019-03-13 17:58:18 +0000

    Added SearchParams component

---
+++ src/SearchParams.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Milwaukee, WI");
+  const [animal, setAnimal] = useState("Dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor={"location"}>
+          Location
+          <input
+            onChange={e => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
