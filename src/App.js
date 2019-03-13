219032855807eec6c7a47f8ae0a86af3d8834360
Christopher Grabinski <christopher@unbabel.com>
2019-03-13 20:49:20 +0000

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const NotFound = () => <h1>Page not found</h1>;
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1>Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+          <NotFound default />
+        </Router>
       </div>
     </React.StrictMode>
   );
